8848371c7ede793b04b848f6e0b6f28d35322ed3
Daniel <nonprivado@gmail.com>
2024-11-12 17:38:17 +0100

    Creada y codificada clase Cliente, Cajero y Producto

--- Supermercado/src/Cajero.java
+++ Supermercado/src/Cajero.java
@@ -1,2 +1,35 @@
-public class Cajero {
+import java.util.concurrent.Semaphore;
+
+public class Cajero implements Runnable{
+
+    private String nombre;
+    private Semaphore sem;
+
+    public Cajero(String nombre){
+        this.nombre = nombre;
+        this.sem = new Semaphore(1); //SOLO PASA UN CLIENTE POR CAJA
+    }
+
+    public void pasarPorCaja(Cliente c) throws InterruptedException{
+        sem.acquire(); //SOLO PASA UN CLIENTE A LA VEZ
+
+        long tempInicio = System.currentTimeMillis(); //COMIENZA TIEMPO DE ATENDER
+        System.out.println("Cajero " + nombre + " empieza a atencer a " + c.getNombre());
+
+        for (Producto p: c.getCompra()){
+            long tempInicioProducto = System.currentTimeMillis(); //COMIENZA TIEMPO EN LOS PRODUCTOS
+            System.out.println("Cajero " + nombre + " empieza a procesar el producto " + p.getNombre() + " de " + c.getNombre());
+
+            Thread.sleep(500); //TIEMPO QUE SIMULA EL PRODUCTO
+
+            System.out.println("Cajero " + nombre + " procesó  el producto " + p.getNombre() + " de " + c.getNombre());
+            sem.release();
+        }
+
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
--- Supermercado/src/Cliente.java
+++ Supermercado/src/Cliente.java
@@ -1,5 +1,46 @@
-public class Cliente {
-    private String nombre;
-    private List<Productos> productosList;
+import java.util.List;
 
+public class Cliente implements Runnable{
+    private String nombre;
+    private List<Producto> compra;
+    private Cajero cajero;
+
+    public Cliente(String nombre, List<Producto> compra){
+        this.nombre = nombre;
+        this.compra = compra;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Producto> getCompra() {
+        return compra;
+    }
+
+    public void setCompra(List<Producto> compra) {
+        this.compra = compra;
+    }
+
+    public Cajero getCajero() {
+        return cajero;
+    }
+
+    public void setCajero(Cajero cajero) {
+        this.cajero = cajero;
+    }
+
+
+    @Override
+    public void run() {
+        try {
+            cajero.pasarPorCaja(this);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
